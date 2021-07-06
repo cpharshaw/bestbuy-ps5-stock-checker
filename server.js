439a6f90e32833119b00605df0a533472258d566
cpharshaw <cpharshaw+git@gmail.com>
2021-07-05 22:38:35 -0400

    console.log in bbPS5Call function as test

--- server.js
+++ server.js
@@ -60,6 +60,8 @@ app.get("/ps5/bestbuy", function (req, res) {
 
   const bbPS5Call = param => {
 
+    console.log("bbPS5Call executed...")
+
     const date = new Date();
     const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     const minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
